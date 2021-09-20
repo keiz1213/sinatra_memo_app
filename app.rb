63184151dfeadb7e0bc47cd5ee24552b4921ac1e
keiz1213 <fukuoka00tsc@gmail.com>
2021-09-21 07:41:12 +0900

    Change the connection from constant to local variable.
    1.DB接続を格納する変数を定数からローカル変数に変えた
    2.各メソッドの引数にDB接続を渡すようにした

--- app.rb
+++ app.rb
@@ -6,33 +6,33 @@ require 'erb'
 require 'cgi/escape'
 require 'pg'
 
-CONNECTION = PG.connect(dbname: 'postgres')
-CONNECTION.exec('CREATE TABLE IF NOT EXISTS mymemos (id serial, title text,content text)')
+db_connection = PG.connect(dbname: 'postgres')
+db_connection.exec('CREATE TABLE IF NOT EXISTS mymemos (id serial, title text,content text)')
 
-def find_memo(id)
-  memos = CONNECTION.exec('SELECT * FROM mymemos')
+def find_memo(id, connection)
+  memos = connection.exec('SELECT * FROM mymemos')
   memos.find { |m| m['id'] == id }
 end
 
-def post_memo(params)
+def post_memo(params, connection)
   title = params['title']
   content = params['content']
-  CONNECTION.exec('INSERT INTO mymemos (title,content) values($1,$2)', [title, content])
+  connection.exec('INSERT INTO mymemos (title,content) values($1,$2)', [title, content])
 end
 
-def delete_memo(id)
-  CONNECTION.exec("DELETE from mymemos where id = #{id}")
+def delete_memo(id, connection)
+  connection.exec("DELETE from mymemos where id = #{id}")
 end
 
-def edit_memo(id)
+def edit_memo(id, connection)
   title = params['title']
   content = params['content']
-  CONNECTION.exec('UPDATE mymemos set title = $1,content =$2 where id = $3', [title, content, id])
+  connection.exec('UPDATE mymemos set title = $1,content =$2 where id = $3', [title, content, id])
 end
 
 get '/' do
   @title = 'My Memo'
-  @memos = CONNECTION.exec('SELECT * FROM mymemos')
+  @memos = db_connection.exec('SELECT * FROM mymemos')
   erb :home
 end
 
@@ -42,13 +42,13 @@ get '/memos/new' do
 end
 
 post '/memos/new' do
-  post_memo(params)
+  post_memo(params, db_connection)
   redirect to('/')
 end
 
 get '/memos/:id' do |id|
   @id = id
-  memo = find_memo(@id)
+  memo = find_memo(@id, db_connection)
   @memo_title = (memo['title'])
   @memo_content = (memo['content'])
   @title = @memo_title
@@ -57,13 +57,13 @@ end
 
 delete '/memos/:id' do |id|
   @id = id
-  delete_memo(@id)
+  delete_memo(@id, db_connection)
   redirect to '/'
 end
 
 get '/memos/:id/edit' do |id|
   @id = id
-  memo = find_memo(@id)
+  memo = find_memo(@id, db_connection)
   @memo_title = (memo['title'])
   @memo_content = (memo['content'])
   @title = @memo_title
@@ -72,6 +72,6 @@ end
 
 patch '/memos/:id' do |id|
   @id = id
-  edit_memo(@id)
+  edit_memo(@id, db_connection)
   redirect to('/')
 end
